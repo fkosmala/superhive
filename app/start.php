3034a8461c5d97156b4715562b077c399f0c5cd8
Florent Kosmala <kosflorent@gmail.com>
2020-06-14 18:02:40 +0200

    Generate Static page & route from html file

--- app/start.php
+++ app/start.php
@@ -4,8 +4,11 @@ namespace App;
 
 use App\Controllers\HomeController;
 use App\Controllers\AdminController;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use DI\Container;
 use Slim\Factory\AppFactory;
+use Slim\Routing\RouteContext;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 use Tuupola\Middleware\HttpBasicAuthentication as BasicAuth;
@@ -18,6 +21,7 @@ $container = new Container();
 $container->set('basedir', __DIR__ . '/../');
 $container->set('themesdir', __DIR__ . '/../public/themes/');
 $container->set('commentsdir', __DIR__ . '/../app/comments/');
+$container->set('pagesdir', __DIR__ . '/../pages/');
 $container->set('configfile', __DIR__ . '/../config.json');
 $container->set('blogfile', __DIR__ . '/../app/blog.json');
 $container->set('password', __DIR__ . '/../password');
@@ -31,9 +35,10 @@ $container->set('settings', function() {
 $container->set('view', function() {
     $twig = Twig::create(
 			[
-				__DIR__ . "/../app/views/", 
+				__DIR__ . "/../app/views/",
+				__DIR__ . "/../pages/",
 				__DIR__ . "/../public/themes/"
-			], 
+			],
 			[
 				'cache' => false
 			]
@@ -83,5 +88,24 @@ $app->get('/sitemap', HomeController::class . ":sitemap")->setName('sitemap');
 // Admin routes
 $app->get('/admin', AdminController::class . ":adminIndex")->setName('admin');
 $app->post('/admin/save', AdminController::class . ":save")->setName('save');
+
+// generate routes from static pages
+$pagesDir = $container->get('pagesdir');
+$pages = preg_grep('~\.(html)$~', scandir($pagesDir));
+
+foreach ($pages as $page) {
+	$route = substr($page, 0, strrpos($page, "."));
+	$app->get('/'.$route, function ($request, $response) {
+		$settings = $this->get('settings');
+		$theme = $settings['theme'].'/layout.html';
+		$uri = $request->getUri();
+		$route = substr(strrchr($uri, "/"), 1);
+		return $this->get('view')->render($response, $route.'.html', [
+			"settings"=>$settings,
+			"theme"=>$theme
+		]);
+	})->setName($route);
+
+}
 
 return $app;
